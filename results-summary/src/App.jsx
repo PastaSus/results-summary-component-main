017d24803c99c9935176380a964ebcdd641233f5
Aaron Cerezo <aarongtxd@gmail.com>
2025-09-21 15:43:06 +0800

    feat(a11y): add main landmark

--- results-summary/src/App.jsx
+++ results-summary/src/App.jsx
@@ -5,10 +5,12 @@ import ResultsSummaryTypes from "./components/ResultsSummaryTypes";
 
 function App() {
   return (
-    <section className="results-summary__component xl:flex xl:max-w-170 xl:justify-center xl:rounded-[2rem] xl:bg-white xl:shadow-[20px_20px_40px_rgba(0_0_0_/_0.1)]">
-      <ResultsSummaryIntro></ResultsSummaryIntro>
-      <ResultsSummaryTypes></ResultsSummaryTypes>
-    </section>
+    <main>
+      <section className="results-summary__component xl:flex xl:max-w-170 xl:justify-center xl:rounded-[2rem] xl:bg-white xl:shadow-[20px_20px_40px_rgba(0_0_0_/_0.1)]">
+        <ResultsSummaryIntro></ResultsSummaryIntro>
+        <ResultsSummaryTypes></ResultsSummaryTypes>
+      </section>
+    </main>
   );
 }
 
